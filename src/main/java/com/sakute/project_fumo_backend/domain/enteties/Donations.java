fc5c5af0becd839a5025fde1c68a34b324ebbd62
Andrii Erdek <andriy.erdek1012@gmail.com>
2024-03-04 19:34:08 +0200

    PROJECT FUMO. V1.0. Initialization of project, creating entities.

--- src/main/java/com/sakute/project_fumo_backend/domain/enteties/Donations.java
+++ src/main/java/com/sakute/project_fumo_backend/domain/enteties/Donations.java
@@ -1,4 +1,49 @@
 package com.sakute.project_fumo_backend.domain.enteties;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name = "donations")
 public class Donations {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "donation_id")
+    private Long id;
+
+    @Column(name = "fundraising_id", nullable = false)
+    private Long fundraisingId;
+
+    @Column(name = "donor_id", nullable = false)
+    private Long donorId;
+
+    @Column(name = "amount", nullable = false)
+    private BigDecimal amount;
+
+    @Column(name = "donation_date", nullable = false)
+    private Timestamp donationDate;
+
+    @Column(name = "payment_method", nullable = false)
+    private String paymentMethod;
+
+    @Column(name = "anonymous", nullable = false)
+    private boolean anonymous;
+
+    @ManyToOne
+    @JoinColumn(name = "donor_id", insertable = false, updatable = false)
+    private User donor;
+
+    @ManyToOne
+    @JoinColumn(name = "fundraising_id", insertable = false, updatable = false)
+    private Fundraising fundraising;
 }
+
