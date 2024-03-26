5d1d0d6b6983525e7f951c0c1493c0ef7c503901
Andrii Erdek <andriy.erdek1012@gmail.com>
2024-03-26 11:26:10 +0200

    Refactored Ids with UUIDs, removed unused imports

--- src/main/java/com/sakute/project_fumo_backend/controller/TopicsController.java
+++ src/main/java/com/sakute/project_fumo_backend/controller/TopicsController.java
@@ -1,6 +1,5 @@
 package com.sakute.project_fumo_backend.controller;
 
-import com.sakute.project_fumo_backend.domain.enteties.PostTagTopic;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/com/sakute/project_fumo_backend/domain/service/dto/UserDto.java
+++ src/main/java/com/sakute/project_fumo_backend/domain/service/dto/UserDto.java
@@ -1,13 +1,14 @@
 package com.sakute.project_fumo_backend.domain.service.dto;
 
 import com.sakute.project_fumo_backend.domain.enteties.Role;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 import java.sql.Timestamp;
 
 @Builder
 public class UserDto {
-
+    @NotNull
     private Long id;
     private String username;
     private String email;
