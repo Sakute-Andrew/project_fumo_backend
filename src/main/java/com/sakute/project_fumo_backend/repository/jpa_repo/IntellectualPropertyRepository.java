033cba9b8b47f9c0c769a63a6683501dc351c037
Sakute <106104940+Sakute-Andrew@users.noreply.github.com>
2026-06-04 10:10:53 +0300

    Remove unnecessary comment from IntellectualPropertyRepository

--- src/main/java/com/sakute/project_fumo_backend/repository/jpa_repo/IntellectualPropertyRepository.java
+++ src/main/java/com/sakute/project_fumo_backend/repository/jpa_repo/IntellectualPropertyRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import java.util.List;
 import java.util.UUID;
 
-// IntellectualPropertyRepository.java
 public interface IntellectualPropertyRepository extends JpaRepository<IntellectualProperty, UUID>, JpaSpecificationExecutor<IntellectualProperty> {
 
     List<IntellectualProperty> findByNameContainingIgnoreCase(String name);
